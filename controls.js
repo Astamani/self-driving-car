57edd32be4ad00d88a31f26c79196c5e912d0c91
Astamani <136489441+Astamani@users.noreply.github.com>
2025-08-27 11:59:25 +0530

    basic mechanism of Car

---
+++ controls.js
@@ -0,0 +1,52 @@
+class Controls {
+  constructor() {
+    this.forward = false;
+    this.reverse = false;
+    this.Lside = false;
+    this.Rside = false;
+
+    this.#addKeyboardListners();
+  }
+
+  #addKeyboardListners() {
+    document.onkeydown = (event) => {
+      switch (event.key) {
+        case "ArrowLeft":
+          this.Lside = true;
+          break;
+        case "ArrowRight":
+          this.Rside = true;
+          break;
+
+        case "ArrowUp":
+          this.forward = true;
+          break;
+
+        case "ArrowDown":
+          this.reverse = true;
+        break
+      }
+      console.table(this);
+    };
+    document.onkeyup = (event) => {
+      switch (event.key) {
+        case "ArrowLeft":
+          this.Lside = false;
+          break;
+
+        case "ArrowRight":
+          this.Rside = false;
+          break;
+
+        case "ArrowUp":
+          this.forward = false;
+          break;
+
+        case "ArrowDown":
+          this.reverse = false;
+          break;
+      }
+      console.table(this);
+    };
+  }
+}
